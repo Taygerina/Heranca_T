f655c7abd155902ed4bb47b50f12455c79ea5c59
Talita <talitamarcelo15@gmail.com>
2021-10-19 19:16:20 -0300

    finalizando a tarefa por completo.

--- Program.cs
+++ Program.cs
@@ -14,13 +14,25 @@ namespace Herança
                 Conta acc1 = bacc;
                 Conta acc2 = new ContaJur(1003, "Trunks", 0.0, 200.00);
                 Conta acc3 = new Contapoupanca(1004, "Vegeta", 0.0, 0.01);
- 
-                Console.WriteLine("Finalizado!");
+                ContaJur acc4 = (ContaJur)acc2;
+                 acc4.Emprestimo(100.0);
 
-
-
-
-
+            //chamando para a tela o emprestimo
+            if (acc3 is ContaJur)
+            {
+                ContaJur acc5 = (ContaJur)acc3;
+                acc5.Emprestimo(200.0);
+                Console.WriteLine("Emprestimo!");
+            }
+            
+            //chamando para a tela a atualização
+            if (acc3 is Contapoupanca)
+            {
+                Contapoupanca acc5 = acc3 as Contapoupanca;
+                acc5.atualizacaoSaldo();
+                Console.WriteLine("Atualizado!");
+            }
+               
         }
     }
 
